bce4fbb5683589504a076ac6229609a5cbbc0517
Charan Raj Musali <cmusali@cmusali.remote.csb>
2024-06-19 22:24:22 +0530

    Initial commit

---
+++ profile-resource-utiization.py
@@ -0,0 +1,22 @@
+# This script basically does one simple thing ...
+# Generates meaningless synthetic data and keeps it the memory. 
+
+
+# We should expect to see while profiling the resource utilization/
+# 1 processor (core) utilization of Compute power 
+# 1GiB utilization of Memory Power
+
+
+#with open('a.log', 'w') as file_des:
+#    for item in range(1024 * 1024):
+#        file_des.write('c' * 1024 + '\n')
+
+import time
+
+synthetic_data = [ 'c' * 1024 + '\n' for item in range(1024 * 1024) ]
+counter = 0
+
+while True:
+    counter += 1
+    print(f'Entered infinite loop ... Seconds elapsed {counter}')
+    time.sleep(1)
